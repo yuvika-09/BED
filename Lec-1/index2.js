7109c3fe6ebed0837cd44296995e0e0312c206a7
Yuvika <cyuvika910@gmail.com>
2025-07-03 15:36:23 +0530

    done

--- Lec-1/index2.js
+++ Lec-1/index2.js
@@ -8,4 +8,29 @@ setTimeout(()=>{
 setImmediate(()=>{
     console.log("Set immediate ")
 })
+fs.readFile("demo.txt" , (err,data)=>{
+    console.log("file read")
+    console.log(data)
+    setTimeout(()=>{
+        console.log("timer 2")
+    },0)
+    setImmediate(()=>{
+        console.log("immediate 2")
+    })
+})
+function someTask() {
+    return new Promise ((resolve,reject)=>{
+        resolve("promise")
+    })
+}
+someTask().then((data)=>{
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
+})
+process.nextTick(()=>{
+    console.log("next tick")
+})
+
 console.log("end");

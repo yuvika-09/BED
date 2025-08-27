5e57ed84952495819379eb4c33e61f96bf8d9d47
Yuvika <cyuvika910@gmail.com>
2025-08-27 09:18:39 +0530

    done

---
+++ Lec-22/server.js
@@ -0,0 +1,172 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken");
+const app = express();  
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const Blog = require('./model/blog');
+const User = require('./model/user');
+
+
+//login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email: email, password: password });
+  if (!user) {
+    return res.json({ success: false, message: "Invalid email or password" });
+  }
+
+  const token = jwt.sign({ userId: user._id }, "okk", { expiresIn: "1h" });
+
+  res.json({
+    success: true,
+    message: "Login successful",
+    token: token
+  });
+});
+
+//middleware to verify jwt token
+function isLogin(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.json({ success: false, message: "No token provided" });
+  }
+
+  const decode = jwt.verify(token, "okk");
+  if (decode && decode.userId) {
+    req.userId = decode.userId;   
+    return next();
+  }
+
+  res.json({ success: false, message: "Invalid token" });
+}
+
+
+
+
+// create
+app.post("/blogs", isLogin,async(req,res)=>{
+    let title = req.body.title;
+    let body = req.body.body;
+    let userId = req.body.userId; 
+     let user = await User.findById(req.userId);
+    if(!user){
+        return res.json({
+            success:false,
+            message:"Invalid user"
+        })
+    }
+    let blog = {
+        title : title,
+        body : body,
+        date : Date.now(),
+        userId: req.userId
+    }
+    let newBlog = new Blog(blog) 
+    await newBlog.save()
+    user.blogs.push(newBlog._id);
+    await user.save();
+    res.json({
+        success : true,
+        message : "blog added successfully",
+        data : newBlog
+    })
+})
+
+
+app.delete("/blogs/:blogId",isLogin,async(req,res)=>{
+    const blogId = req.params.blogId;
+  const blogExist = await Blog.findById(blogId);
+
+  if (!blogExist) {
+    return res.json({ success: false, message: "Blog does not exist" });
+  }
+
+  if (blogExist.userId.toString() !== req.userId) {
+    return res.json({ success: false, message: "Permission denied" });
+  }
+
+  await Blog.findByIdAndDelete(blogId);
+  res.json({ success: true, message: "Blog deleted successfully" });
+})
+
+
+
+// read
+// read all data
+app.get("/blogs",async (req,res)=>{
+    let allBlogs = await Blog.find();
+    res.json({
+        success : true,
+        message : "all data fetched successfully",
+        data : allBlogs
+    })
+})
+// read single data
+app.get("/blogs/:id",async (req,res)=>{
+    let id = req.params.id;
+    let blog = await Blog.findById(id); 
+    res.json({
+        success : true,
+        message : "blog fetched successfully",
+        data : blog
+    })
+})
+
+
+
+// create user
+app.post("/users", async (req,res)=>{
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+    let user = {
+        name : name,
+        email : email,
+        password : password
+    }
+    let newUser = new User(user)
+    await newUser.save()
+    res.json({
+        success : true,
+        message : "user added successfully",
+        data : newUser
+    })
+})
+
+// read
+// read all users
+app.get("/users", async (req,res)=>{
+    let allUsers = await User.find();
+    res.json({
+        success : true,
+        message : "all users fetched successfully",
+        data : allUsers
+    })
+})
+
+// read single user
+app.get("/users/:id", async(req,res)=>{
+    let id = req.params.id;
+    let user = await User.findById(id);
+    res.json({
+        success : true,
+        message : "user fetched successfully",
+        data : user
+    })
+})
+
+mongoose.connect('mongodb://127.0.0.1:27017/G27DBs')
+  .then(() => console.log('Connected!'));
+
+app.listen(5556, () => {
+    console.log("Server is running on http://localhost:5556");
+});
+
+
+// login route and generate Jwt token with payload userId
+// in route post/blogs add middleware isLogin in this middleware verify jwt token and
+//  modify req object like req.userId=decode.userId

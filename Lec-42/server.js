2a5226f1ed61020e4a4985fdb75a299040caf31d
Yuvika <cyuvika910@gmail.com>
2025-12-09 10:58:18 +0530

    done

---
+++ Lec-42/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+})
+
+app.listen(7687, () => {   
+    console.log('Server started'); 
+});
